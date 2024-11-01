22367fff21fa392bd06b2bc9e27c2ceafca8911b
PRAVEEN KUSHWAHA <praveenkushwaha.tech@gmail.com>
2024-11-01 15:20:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        while (tc-- > 0) {
+            String str[] = br.readLine().split(" ");
+            int arr[] = new int[str.length];
+
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            Solution obj = new Solution();
+
+            obj.reverseArray(arr);
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public void reverseArray(int arr[]) {
+    // reverseArray(arr, 0, arr.length - 1);
+        // code here
+        int l=0,h=arr.length-1,temp;
+        
+      while(h>l){
+            temp = arr[l];
+            arr[l] = arr[h];
+            arr[h] = temp;
+         l++;
+         h--;
+      }
+    }
+    
+    public static void reverseArray(int[] arr, int start, int end){
+        if(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            reverseArray(arr, start + 1, end - 1);
+        }
+    }
+}
