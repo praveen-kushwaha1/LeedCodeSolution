319e04b89c06ae1ea0047d2b1e99efcbcd1d97b8
PRAVEEN KUSHWAHA <praveenkushwaha.tech@gmail.com>
2024-11-09 15:37:04 +0530

    Time: 1 ms (100%), Space: 45 MB (6.41%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -9,23 +9,65 @@
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode tail = dummyHead;
-        int carry = 0;
-        while (l1 != null || l2 != null || carry != 0) {
-            int digit1 = (l1 != null) ? l1.val : 0;
-            int digit2 = (l2 != null) ? l2.val : 0;
-            int sum = digit1 + digit2 + carry;
-            carry = sum / 10;
-            ListNode newNode = new ListNode(sum % 10);
-            tail.next = newNode;
-            tail = newNode;
-            l1 = (l1 != null) ? l1.next : null;
-            l2 = (l2 != null) ? l2.next : null;
+    public ListNode addTwoNumbers(ListNode num1, ListNode num2) {
+         // Find the length of both the linked lists
+        int len1 = countNodes(num1);
+        int len2 = countNodes(num2);
+
+        // If num1 is smaller, swap the two linked lists by
+        // calling the function with reversed parameters
+        if (len1 < len2) {
+            return addTwoNumbers(num2, num1);
         }
-        ListNode result = dummyHead.next;
-        dummyHead.next = null;
-        return result;
+
+        int carry = 0;
+       // num1 = reverse(num1);
+        //num2 = reverse(num2);
+
+        ListNode res = num1;
+
+        // Iterate till either num2 is not empty or
+        // carry is greater than 0
+        while (num2 != null || carry != 0) {
+
+            // Add carry to num1
+            num1.val += carry;
+
+            // If num2 linked list is not empty, add it to num1
+            if (num2 != null) {
+                num1.val += num2.val;
+                num2 = num2.next;
+            }
+
+            // Store the carry for the next nodes
+            carry = num1.val / 10;
+
+            // Store the remainder in num1
+            num1.val = num1.val % 10;
+
+            // If we are at the last node of num1 but carry is
+            // still left, then append a new node to num1
+            if (num1.next == null && carry != 0) {
+                num1.next = new ListNode(0);
+            }
+
+            num1 = num1.next;
+        }
+
+        // Reverse the resultant linked list to get the
+        // required linked list
+        return res;
+    }
+
+    // Function to find the length of linked list
+    static int countNodes(ListNode head) {
+        int len = 0;
+        ListNode curr = head;
+
+        while (curr != null) {
+            len += 1;
+            curr = curr.next;
+        }
+        return len;
     }
 }
