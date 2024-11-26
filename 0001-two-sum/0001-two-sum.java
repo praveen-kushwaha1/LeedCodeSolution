8b4051abf9aa872ce090133ef63306abfcf6b555
PRAVEEN KUSHWAHA <praveenkushwaha.tech@gmail.com>
2024-11-26 23:55:34 +0530

    Time: 2 ms (98.88%), Space: 45.1 MB (11.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n=nums.length;
+         int n=nums.length;
         Map<Integer,Integer> map=new HashMap<>();
         int[] result=new int[2];
         for(int i=0;i<n;i++){
