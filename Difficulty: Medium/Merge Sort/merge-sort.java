cf6eaf9276ffa0606457f99d2e23c8ea93686465
PRAVEEN KUSHWAHA <praveenkushwaha.tech@gmail.com>
2025-06-26 15:30:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.java
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -1,83 +1,38 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while (t-- > 0) {
-
-            ArrayList<Integer> array1 = new ArrayList<Integer>();
-            String line = read.readLine();
-            String[] tokens = line.split(" ");
-            for (String token : tokens) {
-                array1.add(Integer.parseInt(token));
-            }
-            ArrayList<Integer> v = new ArrayList<Integer>();
-            int[] arr = new int[array1.size()];
-            int idx = 0;
-            for (int i : array1) arr[idx++] = i;
-
-            new Solution().mergeSort(arr, 0, arr.length - 1);
-
-            for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
-
-            System.out.println();
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
 
+
+
     void mergeSort(int arr[], int l, int r) {
-        if(l >= r){
-            return;
-        }
+        
+        if(l>=r)return ;
+        
         int m=(r-l)/2+l;
         
         mergeSort(arr,l,m);
         mergeSort(arr,m+1,r);
-        merge(arr, l, r);
+        merge(arr,l,m,r);
+        
         
     }
     
-    public static void merge(int []arr,int start,int end){
-        int mid = start + (end - start)/2;
-
-        // creating a temp Array
-        int[] temp = new int[end - start + 1];
+    void merge(int []arr,int l,int m,int r){
         
-        int i = start;
-        int j = mid + 1;
-        int k = 0;
-        while(i <= mid && j <= end){
-            if(arr[i] <= arr[j]){
-                temp[k++] = arr[i++];
-            }
-            else{
-                temp[k++] = arr[j++];
-            }
+        int []temp=new int [r-l+1];
+        
+        int i=l;
+        int j=m+1;
+        int k=0;
+        
+        while(i<=m && j<=r){
+            if(arr[i]<=arr[j])temp[k++]=arr[i++];
+            else temp[k++]=arr[j++];
         }
-        while(i <= mid){
-            temp[k++] = arr[i++];
-        }
-        while(j <= end){
-            temp[k++] = arr[j++];
-        }
-
-        // Copying all Elements of temp into Original Array.
-        for(int m = 0, n = start; m < temp.length; m++, n++){
-            arr[n] = temp[m];
+        
+        while(i<=m)temp[k++]=arr[i++];
+        while(j<=r)temp[k++]=arr[j++];
+        
+        for(m=0;m<temp.length;m++){
+            arr[l+m]=temp[m];
         }
     }
 }
